244765ff44141585f5c41ae56416f3896c1a37a0
Jason Barbier <jason@corrupted.io>
2024-11-11 12:38:51 -0800

    remove zsh:w

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -118,6 +118,41 @@
   };
   xdg.portal.config.common.default = "*";
  programs = {
+    alacritty = {
+      enable = true;
+      settings = {
+        env.TERM = "xterm-256color";
+	colors = {
+	  primary = {
+	    "background" = "#1a1b26";
+            "foreground" = "#a9b1d6";
+	  };
+	  normal = {
+	    "black" = "#32344a";
+            "red" = "#f7768e";
+            "green" = "#9ece6a";
+            "yellow" = "#e0af68";
+            "blue" = "#7aa2f7";
+            "magenta" = "#ad8ee6";
+            "cyan" = "#449dab";
+            "white" = "#787c99";
+	  };
+	  bright = {
+	    "black" = "#444b6a";
+            "red" = "#ff7a93";
+            "green" = "#b9f27c";
+            "yellow" = "#ff9e64";
+            "blue" = "#7da6ff";
+            "magenta" = "#bb9af7";
+            "cyan" = "#0db9d7";
+            "white" = "#acb0d0";
+	  };
+	  selection = {
+	    "background" = "#7aa2f7";
+	  };
+	};
+      };
+    };
     obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [
@@ -139,33 +174,6 @@
       plugins = [
         { name = "fzf"; src = pkgs.fishPlugins.fzf-fish; }
       ];
-    };
-    zsh = {
-      enable = true;
-      history = {
-        size = 10000;
-        path = ".config/zsh/history";
-	expireDuplicatesFirst = true;
-	#ignoreAllDups = true;
-      };
-      enableCompletion = true;
-      enableVteIntegration = true;
-      autocd = true;
-      dotDir = ".config/zsh";
-      shellAliases = {
-        vi = "nvim";
-	vim = "nvim";
-	ll = "ls -c -l";
-	ls = "ls -c";
-	update = "sudo nixos-rebuild switch --flake /etc/nixos";
-      };
-      oh-my-zsh = {
-        enable = true;
-	plugins = [
-	  "git"
-	];
-        theme = "gentoo";
-      };
     };
     git = {
       enable = true;
