16a415af47015e55f356baacb99582de6623db16
Jason Barbier <jason@corrupted.io>
2024-11-27 16:06:43 -0800

    beast tweaks

--- hosts/beast/default.nix
+++ hosts/beast/default.nix
@@ -347,6 +347,17 @@
             enable = true;
             packages = [ pkgs.OVMFFull.fd ];
           };
+          verbatimConfig = ''
+      memory_backing_dir = "/dev/hugepages"
+      nvram = [ "${pkgs.OVMFFull}/FV/OVMF.fd:/run/libvirt/nix-ovmf/OVMF_CODE.fd" ]
+      cgroup_device_acl = [
+        "/dev/null", "/dev/full", "/dev/zero",
+        "/dev/random", "/dev/urandom",
+        "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
+        "/dev/rtc","/dev/hpet",
+        "/dev/vfio/vfio", "/dev/vfio/1"
+      ]
+    '';
         };
       };
     };
