741e17693d32e10c6f87dcaf08d0d9904c24c52a
Jason Barbier <jason@corrupted.io>
2024-12-01 14:27:11 -0800

    add pve

--- hosts/pve/default.nix
+++ hosts/pve/default.nix
@@ -5,7 +5,7 @@
     [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      (modulesPath + "/installer/scan/not-detected.nix"
+      (modulesPath + "/installer/scan/not-detected.nix")
         ];
       nix =
       let
