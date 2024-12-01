df6488a51ee9064ef7442f31cc833d190cc27609
Jason Barbier <jason@corrupted.io>
2024-12-01 15:25:31 -0800

    adding mediaserver role to pve

--- roles/mediaserver/default.nix
+++ roles/mediaserver/default.nix
@@ -10,6 +10,15 @@
       enable = true;
       openFirewall = true;
     };
+    plex = {
+      enable = true;
+      openFirewall = true;
+      accelerationDevices = [
+      "/dev/dri/card0"
+      "/dev/dri/card1"
+      "/dev/dri/renderD128"
+      ];
+    };
   };
   environment.systemPackages = [
     pkgs.jellyfin
