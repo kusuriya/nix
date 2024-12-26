f49a42a6b2975e9b543a69e0fb9b67d23de0a466
Jason Barbier <jason@corrupted.io>
2024-12-26 10:11:47 -0800

    add cosmic back to my flakes

--- flake.nix
+++ flake.nix
@@ -91,6 +91,7 @@
           modules = [
             # Base configuration
             ./hosts/${hostname}
+            nixos-cosmic.nixosModules.default
             # Conditional home-manager setup
             (nixpkgs.lib.mkIf homeManagerConfig {
               home-manager = {
