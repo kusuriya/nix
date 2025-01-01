9c6c38b0b5e9c42de0795e9a459c86438cd9f091
Jason Barbier <jason@corrupted.io>
2024-12-31 18:29:50 -0800

    convert to btrfs due to some bcachefs issues

--- hosts/beast/hardware-configuration.nix
+++ hosts/beast/hardware-configuration.nix
@@ -34,7 +34,8 @@
       "preempt=full"
     ];
   };
-  fileSystems."/" =
+  fileSystems = { 
+    "/" =
     {
       device = "/dev/disk/by-label/root";
       fsType = "btrfs";
@@ -43,14 +44,15 @@
         "noatime"
       ];
     };
-
-  fileSystems."/boot" =
+  "/home".options = ["compress=zstd"];
+  "/nix".options = ["compress=zstd" "noatime"];
+  "/boot" =
     {
       device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
-
+  };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware = {
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
