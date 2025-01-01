6f138a4bd8bb547fd4f8cd1aaa9f24d29f18a2aa
Jason Barbier <jason@corrupted.io>
2024-12-31 17:07:28 -0800

    pin orca-slicer to stable

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -10,12 +10,14 @@
   nix = {
     settings = {
     substituters = [
-      "https://cache.nixos.org"
+        "https://cache.nixos.org"
+        "https://nix-community.cacheix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
-       "https://cosmic.cachix.org/"
+      "https://cosmic.cachix.org/"
     ];
-    trusted-public-keys = [
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
