a2344e44ab5b61a4a8cb8ac65f70cc54882c926c
Jason Barbier <jason@corrupted.io>
2025-01-06 17:31:20 -0800

    albyhub

--- hosts/pve/default.nix
+++ hosts/pve/default.nix
@@ -7,6 +7,7 @@
       (modulesPath + "/installer/scan/not-detected.nix")
       ./samba.nix
       ../../roles/mediaserver
+      ../../roles/lightning
     ];
   nix =
     let
@@ -89,7 +90,7 @@
     extraGroups = [ "wheel" ];
   };
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     zfs
     git
@@ -126,17 +127,9 @@
 
   swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault
-    true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno2.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault
-    "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault
     config.hardware.enableRedistributableFirmware;
 }
