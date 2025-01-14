ce3cad32b74980e5bb9a52da54207840d919bdc4
Jason Barbier <jason@corrupted.io>
2025-01-14 10:18:09 -0800

    add paperless

--- roles/paperlessngx/default.nix
+++ roles/paperlessngx/default.nix
@@ -5,11 +5,11 @@
   ,...
 }:
 {
-  services.paperless {
+  services.paperless = {
     enable = true;
     address = "0.0.0.0";
     consumptionDir = "/dozer-files/files/paperless/import";
     dataDir = "/dozer-files/files/paperless/data";
-    
+    port = "28981"    
   };
 }
