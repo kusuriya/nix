ae2f6943b96aba0a232fb2634c2a86c49e7e73cb
Jason Barbier <jason@corrupted.io>
2025-01-14 11:36:54 -0800

    added ingress-controller role

---
+++ roles/ingress-controller/default.nix
@@ -0,0 +1,25 @@
+{ self
+, lib
+, config
+, pkg
+, ...
+}:
+{
+  virtualisation = {
+    oci-containers.containers = {
+      albyhub = {
+        image = "docker.io/jc21/nginx-proxy-manager:latest";
+        autoStart = true;
+        ports = [ 
+          "80:80"
+          "81:81"
+          "443:443"
+        ];
+        volumes = [ 
+          "npm:/data"
+          "npm-le:/etc/letsencrypt"
+        ];
+      };
+    };
+  };
+}
