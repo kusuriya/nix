e16828239c893d0f6684f11bb4da1bf02419418e
Jason Barbier <jason@corrupted.io>
2025-01-30 08:48:46 -0800

    enable swaync

--- modules/desktop/hyprland/waybar.nix
+++ modules/desktop/hyprland/waybar.nix
@@ -64,16 +64,37 @@
         "hyprland/window"
       ];
       modules-center = [
-        "clock"
       ];
       modules-right = [
         "idle_inhibitor"
+        "custom/notifcation"
         "wireplumber"
         "power-profiles-daemon"
         "network"
         "battery"
         "tray"
+        "clock"
       ];
+      "custom/notifcation" = {
+        tooltip = false;
+        format = "{icon}";
+        format-icons = {
+          notification = "<span foreground='red'><sup></sup></span>";
+          none = "";
+          dnd-notification = "<span foreground='red'><sup></sup></span>";
+          dnd-none = "";
+          inhibited-notification = "<span foreground='red'><sup></sup></span>";
+          inhibited-none = "";
+          dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+          dnd-inhibited-none = "";
+        };
+        return-type = "json";
+        exec-if = "which swaync-client";
+        exec = "swaync-client -swb";
+        on-click = "swaync-client -t -sw";
+        on-click-right = "swaync-client -d -sw";
+        escape = true;
+      };
       "hyprland/workspaces" = {
         format = "{id}:{icon}";
         on-click = "activate";
