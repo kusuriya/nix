348a717a2d1cea643bf4162b7dd231ef7c18e9f7
Jason Barbier <jason@corrupted.io>
2025-08-04 16:09:57 -0700

    .

--- hosts/pve/default.nix
+++ hosts/pve/default.nix
@@ -108,22 +108,6 @@
     netdata = {
       enable = true;
     };
-    minecraft-server = {
-      enable = true;
-      eula = true;
-      openFirewall = true; # Opens the port the server is running on (by default 25565 but in this case 43000)
-      declarative = true;
-      serverProperties = {
-        server-port = 25565;
-        difficulty = 0;
-        gamemode = 1;
-        allow-flight = true;
-        max-players = 5;
-        motd = "Daklandia";
-        pvp = false;
-        allow-cheats = true;
-      };
-    };
     openssh = {
       enable = true;
       openFirewall = true;
