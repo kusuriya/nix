d4aa387016aa4fa709470c325a4c3df53ecdacc5
Jason Barbier <jason@corrupted.io>
2025-10-13 12:52:36 -0700

    redoing beast

--- hosts/beast/hardware-configuration.nix
+++ hosts/beast/hardware-configuration.nix
@@ -31,21 +31,17 @@
       "iommu=pt"
     ];
   };
-  fileSystems = {
-    "/" =
-      {
-        device = "/dev/disk/by-uuid/36d05ddc-0479-4168-a2dc-bddfeda7ff1c";
-        fsType = "btrfs";
-        options = [ "subvol=@" ];
-      };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/8e9d6da6-f89c-4ea7-b2bc-115ec941996f";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
 
-    "/boot" =
-      {
-        device = "/dev/disk/by-uuid/91B4-ECA3";
-        fsType = "vfat";
-        options = [ "fmask=0077" "dmask=0077" ];
-      };
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/1BF9-63A0";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware = {
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
