822bae4d02498a5ea8f5e2cd75410913ac21aa50
Jason Barbier <jason@corrupted.io>
2026-01-06 10:01:29 -0800

    add the labelrange rd560 driver

--- pkgs/rd560/default.nix
+++ pkgs/rd560/default.nix
@@ -1,16 +1,48 @@
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+let
+  # Import the driver package
+  labelrange-rd560 = pkgs.callPackage ./labelrange-rd560.nix { };
+in
 {
-  rd560driver = pkgs.stdenv.mkDerivation {
-    pname = "RD560-driver";
-    version = "1.0";
-    buildInputs = [ pkgs.cups ]; # Needed for cups-genppdupdate if applicable
-    installPhase = ''
-      mkdir -p $out/share/cups/model/
-      cp ./RD560-printer.ppd $out/share/cups/model/RD560-printer.ppd
-      cp ./RD560-printer-AD.ppd $out/share/cups/model/RD560-printer-AD.ppd
-      mkdir -p $out/lib/cups/filter/
-      cp ./labelrange_printer_filter $out/lib/cups/filter/
-    '';
-    #meta = { ... };
+  # Enable CUPS printing service
+  services.printing = {
+    enable = true;
+
+    # Add the driver to CUPS
+    drivers = [ labelrange-rd560 ];
+
+    # Optional: Enable debug logging for troubleshooting
+    # logLevel = "debug";
   };
+
+  # Optional: Declarative printer configuration
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "LabelRange-RD560";
+        location = "Office";
+        description = "Labelrange RD560 Label Printer";
+
+        # For USB connection:
+        deviceUri = "usb://Labelrange/RD560?serial=YOUR_SERIAL";
+
+        # For Bluetooth (after pairing):
+        # deviceUri = "bluetooth://XX:XX:XX:XX:XX:XX";
+
+        # Model should match the PPD filename (run `lpinfo -m` after rebuild to find it)
+        model = "labelrange/RD560.ppd";  # Adjust based on actual PPD name
+
+        ppdOptions = {
+          PageSize = "w72h100";  # Common label size, adjust as needed
+        };
+      }
+    ];
+    ensureDefaultPrinter = "LabelRange-RD560";
+  };
+
+  # For Bluetooth printing support
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 }
+
