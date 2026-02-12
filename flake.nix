947e59d6fb0ff187e7bf4930b5c7401f14c9f838
Jason Barbier <jason@corrupted.io>
2026-02-11 18:37:45 -0800

    Add disko as an input in flake.nix

--- flake.nix
+++ flake.nix
@@ -12,6 +12,8 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
     nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
 
     # Home manager
     home-manager = {
@@ -49,6 +51,7 @@
     , nixos-cosmic
     , nixpkgs-stable
     , nixpkgs-unstable
+    , disko
     , ...
     }@inputs:
     let
