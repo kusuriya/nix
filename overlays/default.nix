3b018c5e900c91232e3aaea31dc111af130904bc
Jason Barbier <jason@corrupted.io>
2026-06-26 13:44:21 -0700

    push

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,8 @@
 # This file defines overlays
 { inputs, ... }: {
-  additions = final: _prev: { };
+  additions = final: _prev: {
+    hermes-desktop-rs = final.callPackage ../pkgs/hermes-desktop-rs.nix { };
+  };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
